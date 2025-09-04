f3a81b1a69978f3639057dffe5870578edb5a6c1
Vikram Aditya <vikramaadi9798@gmail.com>
2025-09-04 20:16:53 +0530

    Initial commit

---
+++ quiz-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-amber-400'>Quiz App</h1>
+        <p className='text-gray-600'>Welcome to the Quiz App! Test your knowledge with various quizzes.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
